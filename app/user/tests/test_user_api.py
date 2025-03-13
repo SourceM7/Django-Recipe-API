9825fbe2462a1537391feb9e74a46b98fc226f98
SourceM7 <mohamadaladlan@yahoo.com>
2025-03-13 14:26:42 +0300

    formatting

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,7 +11,7 @@ def create_user(**params):
     return get_user_model().objects.create_user(**params)
 
 
-class publicUserApiTests(TestCase):
+class PublicUserApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
@@ -40,9 +40,15 @@ class publicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short_error(self):
-        payload = {"email": "test@example.com", "password": "pw", "name": "Test name"}
+        payload = {
+            "email": "test@example.com",
+            "password": "pw",
+            "name": "Test name",
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exists = get_user_model().objects.filter(email=payload["email"]).exists()
+        user_exists = get_user_model().objects.filter(
+            email=payload["email"]
+        ).exists()
         self.assertFalse(user_exists)
