c5f2236534bc42ae3724229bbe211b923af09f0a
SourceM7 <mohamadaladlan@yahoo.com>
2025-03-17 20:40:26 +0300

    Tags Model-Serializer-Views And Tests Added

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,5 +1,5 @@
-from core.models import Recipe
-from rest_framework import viewsets
+from core.models import Recipe, Tag
+from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -23,3 +23,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+    mixins.DestroyModelMixin,
+):
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by("name")
